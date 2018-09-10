d404015edf695c81b049be35bebae1baa5f2c594
Matthew Ritter <mritter2283@gmail.com>
2018-09-10 12:07:48 -0400

    Added the rest of the needed methods (empty) with comments to complete later

--- Sprint1/src/sprint1/Group.java
+++ Sprint1/src/sprint1/Group.java
@@ -6,23 +6,48 @@ public class Group {
 	String title;
 	String description;
 	
-	public Group(String title, String desc, Date date) {
+	public Group(String title, String description, Date date) {
 		this.title = title;
-		this.description = desc;
+		this.description = description;
 		this.dateCreated = date;
 	}
-	
+	//Returns this groups dateCreated
 	public Date getDateCreated() {
 		return dateCreated;
 	}
-
+	//Returns this groups title
 	public String getTitle() {
 		return title;
 	}
-
+	//Returns this groups description
 	public String getDescription() {
 		return description;
 	}
-	
+	//Returns the number of members of this group
+	public int getNumberOfMembers() {
+		return -1;
+	}
+	//Returns the Member of this group that corresponds to the emailAddress
+	public Member getMember(String emailAddress) {
+		
+	}
+	//Return a list fo Members of this group
+	public List<Member> getMembers(){
+		
+	}
+	//Returns all questions that have been asked in this group in the order
+	//that they were asked
+	public List<Question> getQuestions(){
+		
+	}
+	//Returns all answers to all questions that have been asked in this group in the order 
+	//that they were answered
+	public List<Answers> getAnswers(){
+		
+	}
+	//Provides useful information about this group, neatly formatted
+	public String toString() {
+		
+	}
 	
 }
