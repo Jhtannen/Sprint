25317f7a790ba5a32d38b540d07eaefd5826775b
Chuck Millsap <cmmillsap4@gmail.com>
2018-10-25 20:31:03 -0400

    Started/Created PersistanceManager class

---
+++ Sprint1/src/sprint1/PersistanceManager.java
@@ -0,0 +1,17 @@
+package sprint1;
+
+import java.io.ObjectOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class PersistanceManager {
+   
+   public PersistanceManager {
+   }
+   
+   public static save (SiteManager sm, File file) {
+   }
+   
+   public static read (File file) {
+   }
+}
