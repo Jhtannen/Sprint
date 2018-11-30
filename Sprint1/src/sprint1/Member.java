82627997ba600ccf494506b78894ca7722a54e2c
Alexander Amundson <alex.amundson987@gmail.com>
2018-11-30 15:21:47 -0800

    Added method to ensure members can like a post just once

--- Sprint1/src/sprint1/Member.java
+++ Sprint1/src/sprint1/Member.java
@@ -222,11 +222,21 @@ public class Member implements Serializable{
 
 	//Likes the post passed in as an argument and gives 5 points to the poster
 	private void likePost(Post post) {
-		if (this.getGroups().contains(post.getGroup() ) ) {//check if member is in the group in which "post" was posted
+		if (this.getGroups().contains(post.getGroup()) && !alreadyLikedPost(post) ) {//check if member is in the group in which "post" was posted
 			post.addLike(new Like(this));
 			post.getMembership().addPoints(5);
 		}
 	}	
+	
+	//Checks if this member already likes the post
+	private boolean alreadyLikedPost(Post post) {
+		for (Like like: post.likes) {
+			if (like.getUpVoter() == this) {
+				return true;
+			}
+		}
+		return false;
+	}
 
 	//Adds a comment to a post and gives 10 points to the poster
 	private void commentOnPost(Post post, Comment comment) {
